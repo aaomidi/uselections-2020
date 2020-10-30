8eb991f920d5839f524dada507a407b067cc3c03
Amir Omidi <amir@aaomidi.com>
2020-10-29 18:44:12 -0700

    Amir/main file (#3)
    * create main file
    
    * Add the various types

---
+++ election/types.go
@@ -0,0 +1,37 @@
+package election
+
+type Candidate struct {
+	FirstName string
+	LastName  string
+	Party     Party
+}
+
+type Party struct {
+	Name         string
+	Symbol       string
+	Color        string
+	Abbreviation string
+}
+
+type State struct {
+	Name         string
+	Abbreviation string
+}
+
+// Vote is the representation for the votes of a election in a given state
+type Vote struct {
+	Candidate      Candidate
+	State          State
+	Count          int
+	Percentage     int
+	ElectoralVotes int
+}
+
+// StateVote is the representation of the state of voting in a given state
+type StateVote struct {
+	TotalVotes          int
+	TurnoutPercentage   float64
+	ReportingPercentage float64
+	ReportingCount      int
+	TotalPrecincts      int
+}
