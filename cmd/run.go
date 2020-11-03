0c09083d20a7a1fb90733c85fea932f4d4a9d3f2
Vilsol <me@vil.so>
2020-11-04 01:58:41 +0200

    Run bot instance

--- cmd/run.go
+++ cmd/run.go
@@ -1,7 +1,15 @@
 package cmd
 
 import (
+	"github.com/aaomidi/uselections-2020/data"
+	scraper2 "github.com/aaomidi/uselections-2020/scraper"
+	"github.com/aaomidi/uselections-2020/telegram"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func init() {
@@ -12,7 +20,29 @@ var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run the bot",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// TODO Run the bot
+		scraper := scraper2.NPRScraper{}
+		broadcaster := data.Data{}
+		broadcaster.Start(&scraper)
+
+		tg := telegram.New(viper.GetString("token"), viper.GetString("channel"))
+
+		if err := tg.Create(); err != nil {
+			return errors.Wrap(err, "error creating telegram bot")
+		}
+
+		go tg.Start()
+
+		terminate := make(chan os.Signal, 1)
+		signal.Notify(terminate, syscall.SIGINT, syscall.SIGTERM)
+
+		toTerminate := <-terminate
+		for {
+			if toTerminate != nil {
+				tg.Stop()
+				break
+			}
+		}
+
 		return nil
 	},
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/go-redis/redis/v8 v8.3.3
+	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.7.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
