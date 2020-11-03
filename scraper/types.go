139484680c1c9cdb277b319836e79f33992f85d5
Amir Omidi <amir@aaomidi.com>
2020-11-02 20:27:52 -0800

    Scraper -> Channel

--- scraper/types.go
+++ scraper/types.go
@@ -1,7 +1,10 @@
 package scraper
 
-import "github.com/aaomidi/uselections-2020/election"
+import (
+	"context"
+	"github.com/aaomidi/uselections-2020/election"
+)
 
 type Scraper interface {
-	Scrape() []election.Vote
+	Scrape(context context.Context) <-chan election.Vote
 }
