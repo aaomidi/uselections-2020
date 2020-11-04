ca2cd64cecec258d62faacdabb2c892823d2551a
Vilsol <me@vil.so>
2020-11-04 03:43:42 +0200

    Initial TG stuff (#22)

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -1,9 +1,11 @@
 package telegram
 
 import (
-	"github.com/aaomidi/uselections-2020/election"
 	"github.com/aaomidi/uselections-2020/data"
+	"github.com/aaomidi/uselections-2020/election"
 	"github.com/aaomidi/uselections-2020/redis"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	tb "gopkg.in/tucnak/telebot.v2"
 	"strings"
@@ -11,20 +13,21 @@ import (
 )
 
 type Telegram struct {
-	token     string
-	bot       *tb.Bot
-	channelID string
-	channel   *tb.Chat
-	log       *log.Entry
-	redis     *redis.Redis
-	data      *data.Data
+	token       string
+	bot         *tb.Bot
+	channelID   string
+	channel     *tb.Chat
+	log         *log.Entry
+	redis       *redis.Redis
+	data        *data.Data
+	dataChannel chan data.OutgoingUpdate
 }
 
-func New(token string, channelId string, r *redis.Redis, d *data.Data) Telegram {
+func New(token string, channelID string, r *redis.Redis, d *data.Data) Telegram {
 	return Telegram{
 		token:     token,
 		bot:       nil,
-		channelID: channelId,
+		channelID: channelID,
 		log:       log.WithField("source", "telegram"),
 		redis:     r,
 		data:      d,
@@ -33,8 +36,9 @@ func New(token string, channelId string, r *redis.Redis, d *data.Data) Telegram 
 
 func (t *Telegram) Create() error {
 	bot, err := tb.NewBot(tb.Settings{
-		Token:  t.token,
-		Poller: &tb.LongPoller{Timeout: 15 * time.Second},
+		Token:   t.token,
+		Poller:  &tb.LongPoller{Timeout: 15 * time.Second},
+		Verbose: true,
 	})
 	t.bot = bot
 
@@ -56,6 +60,12 @@ func (t *Telegram) Create() error {
 }
 
 func (t *Telegram) Start() {
+	t.dataChannel = make(chan data.OutgoingUpdate)
+
+	t.data.RegisterDataReceiver(t.dataChannel)
+
+	go t.runListener()
+
 	// On inline query
 	t.bot.Handle(tb.OnQuery, t.handleQuery)
 
@@ -113,4 +123,47 @@ func (t *Telegram) handleChosenInlineResult(c *tb.ChosenInlineResult) {
 
 func (t *Telegram) Stop() {
 	t.bot.Stop()
+}
+
+func (t *Telegram) runListener() {
+	for _, s := range election.GetStates() {
+		state, err := t.redis.GetMessageIdForState(t.channel.ID, s.Abbreviation)
+
+		if err != nil || state == 0 {
+			t.log.Infof("sending new message for %s", s)
+
+			menu := &tb.ReplyMarkup{ResizeReplyKeyboard: true}
+
+			menu.InlineKeyboard = [][]tb.InlineButton{
+				{
+					{
+						Text:   "Subscribe?",
+						Unique: "something",
+					},
+				},
+			}
+
+			send, err := t.bot.Send(t.channel, "Hello world, this message is for: "+s.Name, menu)
+
+			if err != nil {
+				panic(errors.Wrap(err, "cba to deal with this rn"))
+			}
+
+			if err := t.redis.SaveMessageIdForState(t.channel.ID, s.Abbreviation, send.ID); err != nil {
+				panic(errors.Wrap(err, "cba to deal with this rn"))
+			}
+
+			time.Sleep(time.Second * 4)
+
+			continue
+		}
+	}
+
+	for {
+		update := <-t.dataChannel
+
+		spew.Dump(update)
+
+		// TODO Messages and UX
+	}
 }
